a80d89c41775134f5c20a048db1584e6fa7633e5
Seif Lotfy <seif.lotfy@gmail.com>
2017-03-19 16:32:44 +0100

    fix unit tests

--- vhc_test.go
+++ vhc_test.go
@@ -19,25 +19,25 @@ func estimateError(got, exp uint64) float64 {
 	return float64(delta) / float64(exp)
 }
 
-func TestVHLL(t *testing.T) {
+func TestVHC(t *testing.T) {
 	max := uint64(100000)
-	vhll, _ := New()
+	vhc, _ := New()
 	r := rand.NewZipf(rand.New(rand.NewSource(0)), 1.1, 1.0, max)
 	dict := map[string]uint64{}
 
 	for uint64(len(dict)) < max {
-		id := fmt.Sprintf("flow-%d", r.Uint64())
-		vhll.Add([]byte(id))
+		id := fmt.Sprintf("flow-%09d", r.Uint64())
+		vhc.Add([]byte(id))
 		dict[id]++
 	}
 
-	for i := uint64(0); i < 100; i++ {
-		id := fmt.Sprintf("flow-%d", i)
-		res := vhll.Count([]byte(id))
+	for i := uint64(0); i < 10; i++ {
+		id := fmt.Sprintf("flow-%09d", i)
+		res := vhc.Count([]byte(id))
 		exact := dict[string(id)]
 		ratio := 100 * estimateError(res, exact)
 		if ratio > 3 {
-			t.Errorf("%d) VHLL Exact %d, got %d which is %.2f%% error", i, exact, res, ratio)
+			t.Errorf("%d) VHC Exact %d, got %d which is %.2f%% error", i, exact, res, ratio)
 		}
 	}
 }
